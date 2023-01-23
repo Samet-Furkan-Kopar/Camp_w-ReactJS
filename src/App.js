a94b7d1dcaa49c206d9991902cbbb6ebd22a7f03
Samet Furkan Kopar <sametkopar465@gmail.com>
2023-01-23 13:27:42 +0300

    ana yapı ve companentler oluşturuldu , listeleme işlemi yapıldı

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+import { Container } from 'semantic-ui-react';
+import './App.css';
+import Dashboard from './layouts/Dashboard';
+import Navi from './layouts/Navi';
+
+
+function App() {
+  return ( 
+    <div className="App">
+     <Navi></Navi>
+     <Container className='main'><Dashboard></Dashboard></Container>
+     
+      
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/layouts/Categories.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Menu } from 'semantic-ui-react'
+export default function Categories() {
+  return (
+    <div>
+      <Container className='main'>
+      <Menu inverted pointing vertical>
+        <Menu.Item
+          name='home'
+          
+        />
+        <Menu.Item
+          name='messages'
+      
+        />
+        <Menu.Item
+          name='friends'
+          
+        />
+      </Menu>
+      </Container>
+        
+    </div>
+  )
+}
